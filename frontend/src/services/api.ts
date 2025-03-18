84c043f82f48b2338fbde2a154965572550d4183
Jhon Estiven <jhonestiveng44@gmail.com>
2025-03-18 01:31:54 -0500

    pequeño cambio

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL ||  "http://localhost:3309/api";
+const API_URL = import.meta.env.VITE_API_URL ??  "http://localhost:3309/api";
+
+console.log("🌍 API URL en uso:", API_URL); // Verifica qué URL está tomando
 
 const api = axios.create({
     baseURL: API_URL,
